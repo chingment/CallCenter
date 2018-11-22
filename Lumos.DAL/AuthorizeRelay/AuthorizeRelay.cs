c664f72f4b01e0470ae261588d2c75dd6c569273
qiuqingwen <qqw@uplink.net.cn>
2018-11-22 23:08:06 +0800

    .

--- Lumos.DAL/AuthorizeRelay/AuthorizeRelay.cs
+++ Lumos.DAL/AuthorizeRelay/AuthorizeRelay.cs
@@ -152,7 +152,7 @@ namespace Lumos.DAL.AuthorizeRelay
             return result;
         }
 
-        public List<SysMenu> GetUserMenus(string pUserId)
+        public List<SysMenu> GetUserMenus(string pUserId,Enumeration.BelongSite belongSite)
         {
             List<SysMenu> listMenu = new List<SysMenu>();
             var model =
@@ -162,7 +162,7 @@ namespace Lumos.DAL.AuthorizeRelay
                  where
                  (from userrole in _db.SysUserRole where rolemenu.RoleId == userrole.RoleId && userrole.UserId == pUserId select userrole.RoleId)
                  .Contains(rolemenu.RoleId)
-                 select rolemenu.MenuId).Contains(menu.Id)
+                 select rolemenu.MenuId).Contains(menu.Id)&& menu.BelongSite== belongSite
                 select menu;
 
 
