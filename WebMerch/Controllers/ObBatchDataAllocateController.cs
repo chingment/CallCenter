764ec1324d67523d69096235b870e37b0b9bfba0
qiuqingwen <chingment@126.com>
2018-12-03 11:18:40 +0800

    .

---
+++ WebMerch/Controllers/ObBatchDataAllocateController.cs
@@ -0,0 +1,68 @@
+﻿using Lumos;
+using Lumos.BLL.Service.Merch;
+using Lumos.Common;
+using Lumos.Entity;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace WebMerch.Controllers
+{
+    public class ObBatchDataAllocateController : OwnBaseController
+    {
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public CustomJsonResult GetList(RupDataBatchGetListByCarIns rup)
+        {
+            var query = (from u in CurrentDb.ObBatchDataAllocate
+                         join b in CurrentDb.ObBatch on u.ObBatchId equals b.Id
+                         where
+                         u.BelongId == this.CurrentUserId
+                         &&
+                         u.MerchantId == this.CurrentMerchantId
+                         select new { u.Id, ObBatchName = b.Name, ObBatchCode = b.Code, u.DataCount, u.AllocatedCount, u.UnAllocatedCount, u.UnUsedCount, u.UsedCount, u.CreateTime });
+
+            int total = query.Count();
+
+            int pageIndex = rup.PageIndex;
+            int pageSize = 10;
+            query = query.OrderByDescending(r => r.CreateTime).Skip(pageSize * (pageIndex)).Take(pageSize);
+
+
+            var list = query.ToList();
+
+            List<object> olist = new List<object>();
+
+            foreach (var item in list)
+            {
+
+                olist.Add(new
+                {
+                    Id = item.Id,
+                    ObBatchName = item.ObBatchName,
+                    ObBatchCode = item.ObBatchCode,
+                    DataCount = item.DataCount,
+                    AllocatedCount = item.AllocatedCount,
+                    UnAllocatedCount = item.UnAllocatedCount,
+                    UnUsedCount = item.UnUsedCount,
+                    UsedCount = item.UsedCount,
+                    CreateTime = item.CreateTime.ToUnifiedFormatDateTime()
+                });
+            }
+
+
+            PageEntity pageEntity = new PageEntity { PageSize = pageSize, TotalRecord = total, Rows = olist };
+
+            return Json(ResultType.Success, pageEntity, "");
+        }
+    }
+}
