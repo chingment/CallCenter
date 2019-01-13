c76f34a893c754f292f07cb63a7ce0ae9e0f74a4
jiecong <jiecong@outlook.com>
2019-01-13 10:41:17 +0800

    .

--- WebMerch/Controllers/CallResultRecordController.cs
+++ WebMerch/Controllers/CallResultRecordController.cs
@@ -81,63 +81,62 @@ u.MerchantId == this.CurrentMerchantId &&
         }
 
 
+        public CustomJsonResult GetCallRecordList(RupCallRecordGetList rup)
+        {
+            var accessUserIds = MerchServiceFactory.User.GetCanAccessUserIds(this.CurrentMerchantId, this.CurrentUserId);
 
-        //        public CustomJsonResult GetList(RupCallRecordGetList rup)
-        //        {
-        //            var accessUserIds = MerchServiceFactory.User.GetCanAccessUserIds(this.CurrentMerchantId, this.CurrentUserId);
-
-        //            var query = (from u in CurrentDb.CallResultRecord
-        //                         where
-        //u.MerchantId == this.CurrentMerchantId &&
-        // accessUserIds.Contains(u.SalesmanId) &&
-        // (rup.CustomerName == null || u.CustomerName.Contains(rup.CustomerName)) &&
-        // (rup.PhoneNumber == null || u.PhoneNumber.Contains(rup.PhoneNumber)) &&
-        // (rup.SalesmanName == null || u.SalesmanName.Contains(rup.SalesmanName)) &&
-        // u.StartTime != null
-        //                         select new { u.Id, u.CustomerId, u.CustomerName, u.SalesmanId, u.SalesmanName, u.RecordFile, u.TimeLength, u.PhoneNumber, u.CreateTime, u.RingTime, u.AnswerTime, u.ByeTime, u.StartTime });
-
-
-        //            int total = query.Count();
-
-        //            int pageIndex = rup.PageIndex;
-        //            int pageSize = 10;
-        //            query = query.OrderByDescending(r => r.CreateTime).Skip(pageSize * (pageIndex)).Take(pageSize);
-
-        //            var list = query.ToList();
-
-        //            List<object> olist = new List<object>();
-
-        //            foreach (var item in list)
-        //            {
-
-        //                string recordFile = "";
-
-        //                if (!string.IsNullOrEmpty(item.RecordFile))
-        //                {
-        //                    recordFile = "http://39.108.86.40" + item.RecordFile;
-        //                }
-
-        //                olist.Add(new
-        //                {
-        //                    Id = item.Id,
-        //                    CustomerId = item.CustomerId,
-        //                    CustomerName = item.CustomerName,
-        //                    SalesmanId = item.SalesmanId,
-        //                    SalesmanName = item.SalesmanName,
-        //                    RecordFile = recordFile,
-        //                    TimeLength = item.TimeLength,
-        //                    PhoneNumber = item.PhoneNumber,
-        //                    RingTime = item.RingTime.ToUnifiedFormatDateTime(),
-        //                    AnswerTime = item.AnswerTime.ToUnifiedFormatDateTime(),
-        //                    ByeTime = item.ByeTime.ToUnifiedFormatDateTime(),
-        //                    StartTime = item.StartTime.ToUnifiedFormatDateTime(),
-        //                });
-        //            }
+            var query = (from u in CurrentDb.CallRecord
+                         where
+u.MerchantId == this.CurrentMerchantId &&
+ accessUserIds.Contains(u.SalesmanId) &&
+ (rup.CustomerName == null || u.CustomerName.Contains(rup.CustomerName)) &&
+ (rup.CustomerPhoneNumber == null || u.CustomerPhoneNumber.Contains(rup.CustomerPhoneNumber)) &&
+ (rup.SalesmanName == null || u.SalesmanName.Contains(rup.SalesmanName)) &&
+ u.StartTime != null
+                         select new { u.Id, u.CustomerId, u.CustomerName, u.SalesmanId, u.SalesmanName, u.RecordFile, u.TimeLength, u.CustomerPhoneNumber, u.CreateTime, u.RingTime, u.AnswerTime, u.ByeTime, u.StartTime });
 
 
-        //            PageEntity pageEntity = new PageEntity { PageSize = pageSize, TotalRecord = total, Rows = olist };
+            int total = query.Count();
 
-        //            return Json(ResultType.Success, pageEntity, "");
-        //        }
+            int pageIndex = rup.PageIndex;
+            int pageSize = 10;
+            query = query.OrderByDescending(r => r.CreateTime).Skip(pageSize * (pageIndex)).Take(pageSize);
+
+            var list = query.ToList();
+
+            List<object> olist = new List<object>();
+
+            foreach (var item in list)
+            {
+
+                string recordFile = "";
+
+                if (!string.IsNullOrEmpty(item.RecordFile))
+                {
+                    recordFile = "http://39.108.86.40" + item.RecordFile;
+                }
+
+                olist.Add(new
+                {
+                    Id = item.Id,
+                    CustomerId = item.CustomerId,
+                    CustomerName = item.CustomerName,
+                    SalesmanId = item.SalesmanId,
+                    SalesmanName = item.SalesmanName,
+                    RecordFile = recordFile,
+                    TimeLength = item.TimeLength,
+                    CustomerPhoneNumber = item.CustomerPhoneNumber,
+                    RingTime = item.RingTime.ToUnifiedFormatDateTime(),
+                    AnswerTime = item.AnswerTime.ToUnifiedFormatDateTime(),
+                    ByeTime = item.ByeTime.ToUnifiedFormatDateTime(),
+                    StartTime = item.StartTime.ToUnifiedFormatDateTime(),
+                });
+            }
+
+
+            PageEntity pageEntity = new PageEntity { PageSize = pageSize, TotalRecord = total, Rows = olist };
+
+            return Json(ResultType.Success, pageEntity, "");
+        }
     }
 }
