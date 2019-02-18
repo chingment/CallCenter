d051fa2623e06fbf76cb49dc7e60119b82252675
jiecong <jiecong@outlook.com>
2019-02-18 17:40:22 +0800

    .

--- WebMerch/Controllers/ReportController.cs
+++ WebMerch/Controllers/ReportController.cs
@@ -166,5 +166,86 @@ namespace WebMerch.Controllers
                 #endregion
             }
         }
+
+        public ActionResult UserAccount(ReportModel model)
+        {
+            StringBuilder sbTable = new StringBuilder();
+            sbTable.Append("<table class='list-tb' cellspacing='0' cellpadding='0'>");
+            sbTable.Append("<thead>");
+            sbTable.Append("<tr>");
+            sbTable.Append("<th>序号</th>");
+            sbTable.Append("<th>账号</th>");
+            sbTable.Append("<th>姓名</th>");
+            sbTable.Append("</tr>");
+            sbTable.Append("</thead>");
+            sbTable.Append("<tbody>");
+            sbTable.Append("{content}");
+            sbTable.Append("</tbody>");
+            sbTable.Append("</table>");
+
+            if (Request.HttpMethod == "GET")
+            {
+                #region GET
+                sbTable.Replace("{content}", "<tr><td colspan=\"3\"></td></tr>");
+
+                model.TableHtml = sbTable.ToString();
+                return View(model);
+
+                #endregion
+            }
+            else
+            {
+                #region POST
+                StringBuilder sql = new StringBuilder(" select  UserName,FullName from  SysUser  a  inner join  SysMerchantUser b on a.Id=b.Id ");
+
+                sql.Append(" where 1=1 and MerchantId='" + this.CurrentMerchantId + "' ");
+
+
+                sql.Append(" order by UserName asc  ");
+
+
+                DataTable dtData = DatabaseFactory.GetIDBOptionBySql().GetDataSet(sql.ToString()).Tables[0].ToStringDataTable();
+                StringBuilder sbTableContent = new StringBuilder();
+                for (int r = 0; r < dtData.Rows.Count; r++)
+                {
+                    sbTableContent.Append("<tr>");
+
+                    sbTableContent.Append("<td>" + (r + 1) + "</td>");
+
+                    for (int c = 0; c < dtData.Columns.Count; c++)
+                    {
+                        string td_value = "";
+
+                        switch (c)
+                        {
+                            default:
+                                td_value = dtData.Rows[r][c].ToString().Trim();
+                                break;
+                        }
+
+                        sbTableContent.Append("<td>" + td_value + "</td>");
+
+                    }
+
+                    sbTableContent.Append("</tr>");
+                }
+
+                sbTable.Replace("{content}", sbTableContent.ToString());
+
+                ReportTable reportTable = new ReportTable(sbTable.ToString());
+
+                if (model.Operate == Enumeration.OperateType.Serach)
+                {
+                    return Json(ResultType.Success, reportTable, "");
+                }
+                else
+                {
+                    NPOIExcelHelper.HtmlTable2Excel(reportTable.Html, "用户账号");
+
+                    return Json(ResultType.Success, "");
+                }
+                #endregion
+            }
+        }
     }
 }
